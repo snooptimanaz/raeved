d2e3aa5fccb39939472d5f6862675135790727d1
snooptimanaz <snoop.timanaz@gmail.com>
2026-09-08 13:33:05 +0200

    backend/src/main/java/com/raever/repository/UserRepository.java erstellen

---
+++ backend/src/main/java/com/raever/repository/UserRepository.java
@@ -0,0 +1,9 @@
+package com.raever.repository;
+import com.raever.model.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+import java.util.Optional;
+
+public interface UserRepository extends JpaRepository<User, Long> {
+    Optional<User> findByEmail(String email);
+    boolean existsByEmail(String email);
+}
