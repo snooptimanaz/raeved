bfa199679c582b2a32cf14e7f74950c309e08541
snooptimanaz <snoop.timanaz@gmail.com>
2026-09-08 13:30:40 +0200

    backend/src/main/java/com/raever/RaeverApplication.java erstellen

---
+++ backend/src/main/java/com/raever/RaeverApplication.java
@@ -0,0 +1,10 @@
+package com.raever;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RaeverApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(RaeverApplication.class, args);
+    }
+}
